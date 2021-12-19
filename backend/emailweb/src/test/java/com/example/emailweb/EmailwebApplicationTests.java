7326cafcca63d2923af70bf7aecd05c3d4e51e33
aa <ao1228340764a@gmail.com>
2021-12-19 15:13:27 +0200

    intialize

---
+++ backend/emailweb/src/test/java/com/example/emailweb/EmailwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
